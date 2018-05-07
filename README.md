6f0447a824f0ff07e2dbc5f6bd229cc67f08b7c7
Khaled Sarhan <khaled_sarhan2003@yahoo.com>
2018-05-07 20:04:09 +0300

    Activate the game board with applying player rules

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+This is the 3rd project in Udacity's Frontend Web Developer Nanodegree curriculum. The purpose of this project is to showcase a student's Object-Oriented JavaScript and HTML5 Canvas skills. Of course, it's also to create a game that is creative, playable, bug free and fun to play!
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Player Movement
+===============
+
+Move the player using the arrow keys: up, down, left, and right.
+
+Defeating Enemies
+=================
+
+When the player is hit by an enemy, a game life is deducted and the player is sent back to home position on the board.
+
+Learning Resources
+=================
+
+Object-Oriented JavaScript by [Udacity](https://www.udacity.com/course/object-oriented-javascript--ud015).
+HTML5 Canvas by [Udacity](https://www.udacity.com/course/html5-canvas--ud292).
--- js/app.js
+++ js/app.js
@@ -1,40 +1,106 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+//Global variables to calculate the proper position for the player while it's moving
+const cellWidth = 101;
+const cellHeight = 83;
+const numRows = 5;
+const numCols = 4;
+
+let gameLevel = 3;
+
+// Enemies our player must avoid
+var Enemy = function () {
+    // Variables applied to each of our instances go here,
+
+    // The sprite is the image for our enemies
     this.sprite = 'images/enemy-bug.png';
+
+    // Calculate the position and the speed for the ememy randomly
+    this.row = Math.floor(Math.random() * 3) + 1
+    this.x = -1 * cellWidth;
+    this.y = this.row * cellHeight - 20;
+    this.speed = Math.floor(Math.random() * 10) + 3;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+    this.x += this.speed + dt;
+
+    // Collision happens here
+    let currentX = this.x + 70;
+    if (currentX >= player.x && currentX < player.x + cellWidth) {
+        if (this.row == player.row) {
+            player = new Player();
+        }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// A player object to avoid Enemies
+var Player = function () {
+    // Variables applied to each of our instances go here,
 
+    this.sprite = 'images/char-boy.png';
+
+    // These variables for setting the player position
+    this.x = 0;
+    this.y = 0;
+    this.col = 2;
+    this.row = 4;
+};
+
+// Update the player's position, required method for game
+Player.prototype.update = function (dt) {
+    this.x = this.col * cellWidth;
+    this.y = this.row * cellHeight - 20;
+};
+
+// Function to calculate the proper (x,y) and draw the player again and again.
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Function to handle the column and row changes according to the direction of the movement.
+Player.prototype.handleInput = function (key) {
+    col = this.col;
+    row = this.row;
+    switch (key) {
+        case "left":
+            this.col = col == 0 ? col : col - 1;
+            break;
+        case "up":
+            this.row = row == 0 ? row : row - 1;
+            break;
+        case "right":
+            this.col = col == numCols ? col : col + 1;
+            break;
+        case "down":
+            this.row = row == numRows ? row : row + 1;
+            break;
+    }
+    if (row == 1) {
+        reset();
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+let player = new Player();
 
-
+// Add new Enemy to the board and set the timer according to the level of the game
+setInterval(() => {
+    allEnemies.push(new Enemy(), );
+}, gameLevel * 200);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -44,3 +110,8 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function reset() {
+    allEnemies = [];
+    player = new Player();
+}
